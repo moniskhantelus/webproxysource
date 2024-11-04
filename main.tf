5be9010001ec506f3c953e5e0e65378c5590c691
moniskhantelus <93679325+moniskhantelus@users.noreply.github.com>
2024-11-04 00:24:15 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,29 @@ locals {
   create_url_lists = { for k, v in var.policy_rules.url_lists : v.url_list => v if v.values != null }
 }
 
+locals {
+  swp_tag_value = google_tags_tag_value.swp.id
+}
+
+# Create a tag key
+resource "google_tags_tag_key" "swp_tag_key" {
+  parent      = "projects/${var.project_id}"
+  short_name  = "swp_key"
+  description = "Firewall tag key"
+  purpose     = "GCE_FIREWALL"
+  purpose_data = {
+  network = "${var.project_id}/${var.network}"
+}
+  }
+
+
+# Create tag values associated with the key
+resource "google_tags_tag_value" "swp" {
+  parent      = google_tags_tag_key.swp_tag_key.id
+  short_name  = "swp"
+  description = "swp environment"
+}
+
 resource "google_network_security_gateway_security_policy" "policy" {
   provider              = google-beta
   project               = var.project_id
@@ -50,7 +73,6 @@ resource "google_network_security_url_lists" "url_lists" {
   description = coalesce(each.value.description, var.description)
   values      = each.value.values
 }
-
 resource "google_network_security_gateway_security_policy_rule" "url_list_rules" {
   for_each                = var.policy_rules.url_lists
   provider                = google-beta
@@ -61,18 +83,41 @@ resource "google_network_security_gateway_security_policy_rule" "url_list_rules"
   gateway_security_policy = google_network_security_gateway_security_policy.policy.name
   enabled                 = each.value.enabled
   priority                = each.value.priority
-  session_matcher = trimspace(<<-EOT
+  session_matcher = join(" && ", [
+    trimspace(<<-EOT
     inUrlList(host(), '%{~if each.value.values != null~}
     ${~google_network_security_url_lists.url_lists[each.value.url_list].id~}
     %{~else~}
     ${~each.value.url_list~}
-    %{~endif~}') %{~if each.value.session_matcher != null} && (${each.value.session_matcher})%{~endif~}
+    %{~endif~}') 
   EOT
-  )
+    ),
+    "destination.port == ${each.value.port}",
+    "source.matchTag('${local.swp_tag_value}')"
+  ])
+ 
   application_matcher    = each.value.application_matcher
   tls_inspection_enabled = each.value.tls_inspection_enabled
   basic_profile          = each.value.action
 }
+# resource "google_network_security_gateway_security_policy_rule" "url_list_rules" {
+#   for_each                = var.url_lists
+#   provider                = google-beta
+#   project                 = var.project_id
+#   name                    = replace(each.key, "/[^a-z0-9-]/", "") # Ensures valid resource name
+#   location                = var.region
+#   description             = lookup(each.value, "description", "Default description")
+#   gateway_security_policy = google_network_security_gateway_security_policy.policy.name
+#   enabled                 = lookup(each.value, "enabled", true)
+#   priority                = each.value.priority
+
+#   # Apply the dynamically constructed session_matcher
+#   session_matcher         = local.session_matchers[each.key]
+
+#   application_matcher     = lookup(each.value, "application_matcher", null)
+#   tls_inspection_enabled  = lookup(each.value, "tls_inspection_enabled", false)
+#   basic_profile           = lookup(each.value, "action", "ALLOW")
+# }
 
 resource "google_network_security_gateway_security_policy_rule" "custom_rules" {
   for_each                = var.policy_rules.custom
